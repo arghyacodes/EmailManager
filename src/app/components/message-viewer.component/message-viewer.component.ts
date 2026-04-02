952a6b79758f3d5e6255da913362a80b8b8f0abb
arghyacodes <arghyaghoshal.juetce@gmail.com>
2026-04-02 18:35:34 +0530

    adder sorting and searching

--- src/app/components/message-viewer.component/message-viewer.component.ts
+++ src/app/components/message-viewer.component/message-viewer.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../../services/api.service';
 import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-message-viewer',
@@ -17,6 +18,11 @@ export class MessageViewerComponent {
   messages$!: Observable<any[]>;
   selectedMessage: any;
 
+  // sorting and searching
+  searchText = '';
+  sortField: 'time' | 'id' = 'time';
+  sortAsc = false;
+
   constructor(private api: ApiService) { }
 
   ngOnInit() {
@@ -30,11 +36,13 @@ export class MessageViewerComponent {
   }
 
   onTopicChange() {
-    if (!this.selectedTopic) {
-      this.messages$ = this.api.getAllMessages();
-    } else {
-      this.messages$ = this.api.getMessages(this.selectedTopic);
-    }
+    const source$ = !this.selectedTopic
+      ? this.api.getAllMessages()
+      : this.api.getMessages(this.selectedTopic);
+
+    this.messages$ = source$.pipe(
+      map(res => this.processMessages(res))
+    );
 
     this.selectedMessage = null;
   }
@@ -44,9 +52,64 @@ export class MessageViewerComponent {
   }
 
   loadAllMessages() {
-    this.messages$ = this.api.getAllMessages();
+    this.messages$ = this.api.getAllMessages().pipe(
+      map(res => this.processMessages(res))
+    );
   }
   trackById(index: number, item: any) {
     return item.id + (item.topic || '');
+  }
+
+  // process manager
+  processMessages(res: any[]) {
+    let data = (res || []).map(m => ({
+      ...m,
+      time: new Date(m.time)
+    }));
+
+    // SEARCH
+    if (this.searchText) {
+      const search = this.searchText.toLowerCase();
+
+      data = data.filter(m =>
+        m.message?.toLowerCase().includes(search) ||
+        m.details?.toLowerCase().includes(search)
+      );
+    }
+
+    // SORT
+    data.sort((a, b) => {
+      let valA = a[this.sortField];
+      let valB = b[this.sortField];
+
+      if (this.sortField === 'time') {
+        valA = new Date(valA).getTime();
+        valB = new Date(valB).getTime();
+      }
+
+      return this.sortAsc ? valA - valB : valB - valA;
+    });
+
+    return data;
+  }
+
+  // sort toggle
+  toggleSort(field: 'time' | 'id') {
+    if (this.sortField === field) {
+      this.sortAsc = !this.sortAsc;
+    } else {
+      this.sortField = field;
+      this.sortAsc = false;
+    }
+
+    this.refreshData();
+  }
+
+  refreshData() {
+    this.onTopicChange();   // reuse logic
+  }
+
+  onSearchChange() {
+    this.refreshData();
   }
 }
